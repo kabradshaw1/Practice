52898ebe01b2f077e6fa2244af064062adbbfa50
kabradshaw1 <itllbok82@gmail.com>
2023-01-25 10:37:06 -0500

    now completing CRUD requests

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,10 +1,14 @@
-function List(){
-
+function List(props){
+  function handleClick(){
+    props.deletion(props.id)
+  }
   return (
       <div className="note">
-
+        <h1 >  Title: {props.title} </h1>
+        <p > Content: {props.content}</p>
+        <button onClick={handleClick}>Delete</button>
       </div>
   )
 }
 
-export default List
+export default List;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './css/index.css'
 import App from './components/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
