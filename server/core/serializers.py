b1b3b7ae5e11f4d32e1d81b6b9cdcc4686b0fbaa
kabradshaw1 <itllbok82@gmail.com>
2023-01-26 11:09:09 -0500

    trying to restart

--- server/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Notes
-
-class NoteSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Notes
-    fields = ('id', 'title', 'content')
